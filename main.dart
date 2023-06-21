02e869f057ee9f8e7abc4cb8acf3373579e6b36a
TanzilaTania <tanzilatania31@gmail.com>
2023-06-21 11:43:39 +0600

    Done

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ShoppingListPage(),
+    );
+  }
+}
+
+class ShoppingListPage extends StatelessWidget {
+  final List<String> shoppingItems = [
+    'Apples',
+    'Bananas',
+    'Bread',
+    'Milk',
+    'Eggs',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Shopping List'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Cart is empty')),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: Text(shoppingItems[index]),
+          );
+        },
+      ),
+    );
+  }
+}
